b6e9516decc816dd396a13fc8c820d903db5a89b
James Mccarthy <mccarthyjames35@yahoo.com>
2024-06-19 00:07:13 -0700

    server set up

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Welcome from home, test')
-})
+  res.send('Welcome from home, test');
+});
+
 app.get('/books', (req, res) => {
-  res.send('Welcome from books')
-})
+  res.send('Welcome from books');
+});
 
 app.listen(8000, () => {
-  console.log('server is listening')
-})
+  console.log('server is listening on port 8000');
+});
