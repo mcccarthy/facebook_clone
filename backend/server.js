07b0b7b416226f0049d04d0d5d35148ef5fca26c
James Mccarthy <mccarthyjames35@yahoo.com>
2024-06-19 13:40:17 -0700

    added mongo db and succesfull connecton

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const { readdirSync } = require('fs');
 const app = express();
 app.use(cors());
 
+//routes
 readdirSync('./routes').map((r) => app.use('/', require('./routes/' + r)))
 
-app.listen(8000, () => {
-  console.log('server is listening on port 8000');
+//database
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true, // Remove deprecated options
+})
+  .then(() => console.log('DB connected successfully'))
+  .catch((err) => console.log('DB connection failed', err))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
